8858bd64693806e0a1461f75eb391edeae5cf203
Guillermo Soto <sirgasoga@gmail.com>
2019-02-20 13:27:25 +0100

    Contiene3

--- test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
@@ -22,19 +22,28 @@ public class TestFizzBuzz {
         String resultado = fb.getFizzBuzz(2);
         assertEquals("2", resultado);
     }
+    @Test
     public void test3devuelveFizz(){
         FizzBuzz fb = new FizzBuzz();
         String resultado = fb.getFizzBuzz(3);
         assertEquals("Fizz", resultado);
     }
+    @Test
     public void test5devuelveBuzz(){
         FizzBuzz fb = new FizzBuzz();
         String resultado = fb.getFizzBuzz(5);
         assertEquals("Buzz", resultado);
     }
+    @Test
     public void test15devuelveFizzBuzz(){
         FizzBuzz fb = new FizzBuzz();
         String resultado = fb.getFizzBuzz(15);
         assertEquals("FizzBuzz", resultado);
+    }
+    @Test
+     public void test3contiene3(){
+        FizzBuzz fb = new FizzBuzz();
+        String resultado = fb.getFizzBuzz(13);
+        assertEquals("Fizz", resultado);
     }
 }
