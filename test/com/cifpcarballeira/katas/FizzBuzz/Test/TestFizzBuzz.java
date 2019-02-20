40bde9604a6eecef29be779dcf5bfa2f475f6763
Simon Martinez <simonaxe11@gmail.com>
2019-02-20 09:54:33 +0100

    Primer commit

---
+++ test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
@@ -0,0 +1,13 @@
+package com.cifpcarballeira.katas.FizzBuzz.Test;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestFizzBuzz {
+    
+    public TestFizzBuzz() {
+        
+    }
+
+}
