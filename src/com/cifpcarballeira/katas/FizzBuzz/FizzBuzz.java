56b80d322c8ee6942b35b50d4db35887fb95dfe7
Simon Martinez <simonaxe11@gmail.com>
2019-02-20 10:13:15 +0100

    1 devuelve 1

---
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,9 @@
+
+package com.cifpcarballeira.katas.FizzBuzz;
+
+public class FizzBuzz {
+    
+    public String getFizzBuzz(int i){
+        return "1";
+    }
+}
--- test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
@@ -1,13 +1,19 @@
 package com.cifpcarballeira.katas.FizzBuzz.Test;
 
-
+import com.cifpcarballeira.katas.FizzBuzz.FizzBuzz;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestFizzBuzz {
-    
+
     public TestFizzBuzz() {
-        
+
     }
 
+    @Test
+    public void test1devuelve1() {
+        FizzBuzz fb = new FizzBuzz();
+        String resultado = fb.getFizzBuzz(1);
+        assertEquals("1", resultado);
+    }
 }
